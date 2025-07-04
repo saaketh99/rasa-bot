c1f523549fb28c4f2dd01d99d0c24c0e483da11f
saaketh99 <76866541+saaketh99@users.noreply.github.com>
2025-07-04 12:39:13 +0530

    save files

--- actions/actions/actions.py
+++ actions/actions/actions.py
@@ -601,3 +601,12 @@ class ActionTopPincodesByCustomer(Action):
 
         print(f"[TIME] action_top_pincodes_by_customer took {(time.time() - start_time):.2f} seconds")
         return []
+    
+class ActionDefaultFallback(Action):
+    def name(self):
+        return "action_default_fallback"
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message(text="I'm sorry, I didn't quite understand that. Can you rephrase or ask something else?")
+        return []
+
