3b8092b5e5ddd95202b6617caf2cc0865c5dacd7
saaketh99 <76866541+saaketh99@users.noreply.github.com>
2025-07-09 11:57:43 +0530

    mongo add

--- app/db.py
+++ app/db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv:ordersDbAdmin:LuiQu4KLLM0KXvQX//@orders-cluster.jbais.mongodb.net/")
-db = client["orders-db"]  # You can name this whatever you want
-sessions_collection = db["user_sessions"]  # This is your “file” or table-like collection
+client = MongoClient("mongodb+srv://ordersDbAdmin:LuiQu4KLLM0KXvQX@orders-cluster.jbais.mongodb.net/orders-db?retryWrites=true&w=majority")
+db = client["orders-db"]  
+sessions_collection = db["user_sessions"] 
