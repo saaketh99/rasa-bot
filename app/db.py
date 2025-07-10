92bf5a83f7251fcfd3a8f9bc9ffb7b413fe6b25f
saaketh99 <76866541+saaketh99@users.noreply.github.com>
2025-07-10 12:21:16 +0530

    db cahnge

--- app/db.py
+++ app/db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://ordersDbAdmin:LuiQu4KLLM0KXvQX@orders-cluster.jbais.mongodb.net/orders-db?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://ordersDbAdmin:LuiQu4KLLM0KXvQX@orders-cluster.jbais.mongodb.net/?retryWrites=true&w=majority&appName=orders-cluster")
 db = client["orders-db"]  
 sessions_collection = db["user_sessions"] 
